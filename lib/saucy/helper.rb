9717d17d25e6f67d41f329623542c3ed9fd94f58
Alex MacCaw <maccman@gmail.com>
2008-12-04 13:19:33 +0000

    Sprites almost complete

--- lib/saucy/helper.rb
+++ lib/saucy/helper.rb
@@ -5,38 +5,58 @@ module Saucy
     # saucy_tag(name, :option1 => 'foo')
     # saucy_tag(name1, name2, :option2 => 'foo', :hover => {:color => 'blue'})
     
-    def saucy_tag(*args)
+    def saucy_tag(*args, &block)
       options   = args.extract_options!
       texts     = args
       filename  = [
           Digest::MD5.hexdigest(texts.to_s + options.to_s),
           texts.collect(&:underscore)
-      ].flatten.join('_')
+      ].flatten.join('_') + '.png'
       
-      unless File.exists?(File.join(AB_OUTPUT_DIR, filename))
-         Saucy::Render.render(texts, filename, options)
+      unless File.exists?(File.join(ABS_OUTPUT_DIR, filename))
+        # Reverse texts so the normal
+        # state is the last one
+        Saucy::Render.render(texts.reverse, filename, options)
       end
       
       size = Saucy::Image.cached_png_size(filename)
-      src  = File.join(OUTPUT_DIR, 'filename')
+      # We divide by the number of images to get the height
+      # of the first one (for sprites)
+      real_height = (size[1] / args.length)
       
-      style ||= {}
-      style['text-indent'] = '-5000px;'
-      
-      style['background'] = "url(#{src.inspect}) no-repeat;"
-      style['width']      = "#{size[0]}px"
-      style['height']     = "#{size[1]}px"
-      
-      trans = !style.has_key?('background') || style['background'] == "transparent"
-      if trans || options[:transparent]
-        style['_background']    = 'transparent';
-        style['_filter:progid'] = "DXImageTransform.Microsoft.AlphaImageLoader(src=#{src.inspect}, sizingMethod='crop');"
-      end
+      src  = File.join(OUTPUT_DIR, filename)
+      src  = "'#{src}'"
       
       options[:html] ||= {}
-      options[:html][:style] = style.collect {|key, value| [key, value].join(':') }.join(';')
+      options[:html][:class] ||= []
+      style = options[:html][:style] ||= {}
       
-      content_tag_string(options[:tag] || 'div', nil, options[:html])    
+      style['text-indent'] = '-5000px'
+      style['background'] = "url(#{src}) 0 -#{size[1] - real_height}px no-repeat"
+      style['width']      = "#{size[0]}px"
+      style['height']     = "#{real_height}px"
+      
+      trans = !style.has_key?('background') || style['background'] == "transparent"
+      
+      options[:html][:class] << 'saucy'
+      if texts.length > 1
+        options[:html][:class] << 'saucySprite'
+      end
+      
+      options[:tag] ||= :a
+      # A's need to be display:block for text-indent to work
+      options[:html][:style]['display'] = 'block' if options[:tag] == :a
+      
+      options[:html][:style] = style.collect {|key, value| [key, value].join(':') }.join(';')
+      options[:html][:class] = options[:html][:class].join(' ')
+      options[:html].delete(:class) if options[:html][:class].blank?
+
+      
+      if block_given?
+        concat(content_tag(options[:tag] || :a, capture(&block), options[:html] || {}))
+      else
+        content_tag(options[:tag] || :a, options[:content], options[:html] || {})
+      end
     end
 
   end
--- lib/saucy/render.rb
+++ lib/saucy/render.rb
@@ -34,130 +34,132 @@ module Saucy
       }
     }
     
-    def render(texts, filename, options)
-      style = DEFAULT_STYLE.merge(style)    
+    class << self    
+      def render(texts, filename, options = {})
+        style = DEFAULT_STYLE.merge(options[:style] || {})    
       
-      images = Magick::ImageList.new
-      texts.each do |text|
-        images << draw(text,  
-                     DEFAULT_STYLE[:font].merge(style[:font]), 
-                     style[:background] || DEFAULT_STYLE[:background], 
-                     DEFAULT_STYLE[:stroke].merge(style[:stroke]),
-                     DEFAULT_STYLE[:spacing].merge(style[:spacing])
-                  )
-      end
-      
-      # if style[:shadow] && style[:shadow][:render]
-      #   images << shadow_render(image, DEFAULT_STYLE[:shadow].merge(style[:shadow]) )
-      # end
-      # 
-      # if style[:rotate] != 0
-      #   images << rotate_render(image, style[:rotate]) 
-      # end
-      
-      images.append(true) # append vertically
-      FileUtils.mkdir_p(File.join(ABS_OUTPUT_DIR, filename))
-      images.write(File.join(ABS_OUTPUT_DIR, filename))
-    end
-    
-    def draw(text, font, background, stroke, spacing)
-      lines = text.split("\n")
-
-      width = font[:size] * text.length + stroke[:width] * 2
-      height = (font[:size] * 2 + stroke[:width] * 2) * lines.length
-      
-      rvg = Magick::RVG.new(width, height) do |canvas|
-        canvas.background_fill = background if background != 'transparent'
-        
-        sw = stroke[:width]
-        
-        font_file = font[:font].match(/\./) ? File.join(FONT_STORE, font[:font]) : font[:font]
-
-        styles = {
-          :font             =>  '"' + font_file + '"',
-          :font_size        =>  font[:size], 
-          :fill             =>  font[:color], 
-          :font_stretch     =>  font[:stretch],
-          :letter_spacing   =>  spacing[:letter],
-          :word_spacing     =>  spacing[:word],
-          :glyph_orientation_horizontal => font[:rotate]
-        }
-        
-        if stroke[:width] > 0
-          styles.merge!(:stroke => stroke[:color], :stroke_width => stroke[:width])
-    	  end
-        
-        line_height = font[:height] || font[:size]
-        y = 0
-
-        lines.each do |line|
-          canvas.text(sw,font[:size] + y, line).styles(styles)
-
-          if stroke[:inner] && stroke[:width] > 1
-            inner = styles.merge(:stroke_width => 1, :stroke => font[:color])
-            canvas.text(sw,font[:size] + y, line).styles(inner)
-          end
-          y += line_height
+        images = Magick::ImageList.new
+        texts.each do |text|
+          images << draw(text,  
+                       DEFAULT_STYLE[:font].merge(style[:font]), 
+                       style[:background] || DEFAULT_STYLE[:background], 
+                       DEFAULT_STYLE[:stroke].merge(style[:stroke]),
+                       DEFAULT_STYLE[:spacing].merge(style[:spacing])
+                    )
         end
+      
+        # if style[:shadow] && style[:shadow][:render]
+        #   images << shadow_render(image, DEFAULT_STYLE[:shadow].merge(style[:shadow]) )
+        # end
+        # 
+        # if style[:rotate] != 0
+        #   images << rotate_render(image, style[:rotate]) 
+        # end
+        
+        # Make saucy dir
+        FileUtils.mkdir_p(ABS_OUTPUT_DIR)
+        
+        # Append vertically
+        images.append(true).write(File.join(ABS_OUTPUT_DIR, filename))
       end
-      
-      rvg.draw.trim
-    end
     
-    def shadow_render(input, shadow)
-      width   = input.columns + shadow[:blur] * 4 + shadow[:left].ab * 2
-      height  = input.rows    + shadow[:blur] * 4 + shadow[:top].abs * 2
+      def draw(text, font, background, stroke, spacing)
+        lines = text.split("\n")
+
+        width = font[:size] * text.length + stroke[:width] * 2
+        height = (font[:size] * 2 + stroke[:width] * 2) * lines.length
+      
+        rvg = Magick::RVG.new(width, height) do |canvas|
+          canvas.background_fill = background if background != 'transparent'
+        
+          sw = stroke[:width]
+        
+          font_file = font[:font].match(/\./) ? File.join(FONT_STORE, font[:font]) : font[:font]
+
+          styles = {
+            :font             =>  font_file.inspect,
+            :font_size        =>  font[:size], 
+            :fill             =>  font[:color], 
+            :font_stretch     =>  font[:stretch],
+            :letter_spacing   =>  spacing[:letter],
+            :word_spacing     =>  spacing[:word],
+            :glyph_orientation_horizontal => font[:rotate]
+          }
+        
+          if stroke[:width] > 0
+            styles.merge!(:stroke => stroke[:color], :stroke_width => stroke[:width])
+      	  end
+        
+          line_height = font[:height] || font[:size]
+          y = 0
+
+          lines.each do |line|
+            canvas.text(sw,font[:size] + y, line).styles(styles)
+
+            if stroke[:inner] && stroke[:width] > 1
+              inner = styles.merge(:stroke_width => 1, :stroke => font[:color])
+              canvas.text(sw,font[:size] + y, line).styles(inner)
+            end
+            y += line_height
+          end
+        end
+      
+        rvg.draw.trim
+      end
+    
+      def shadow_render(input, shadow)
+        width   = input.columns + shadow[:blur] * 4 + shadow[:left].ab * 2
+        height  = input.rows    + shadow[:blur] * 4 + shadow[:top].abs * 2
      
-      input.matte = true
+        input.matte = true
       
-      opacity_color = "rgb(#{shadow[:opacity]},#{shadow[:opacity]},#{shadow[:opacity]})"
-      input_colorized = input.copy.colorize(1.0, 1.0, 1.0, opacity_color)
+        opacity_color = "rgb(#{shadow[:opacity]},#{shadow[:opacity]},#{shadow[:opacity]})"
+        input_colorized = input.copy.colorize(1.0, 1.0, 1.0, opacity_color)
       
-      shadow_mask = Magick::Image.new(width, height){
-        self.background_color = '#fff'
-      }
+        shadow_mask = Magick::Image.new(width, height){
+          self.background_color = '#fff'
+        }
       
-      zero = [shadow[:left].abs + shadow[:blur], shadow[:top].abs + shadow[:blur]]
+        zero = [shadow[:left].abs + shadow[:blur], shadow[:top].abs + shadow[:blur]]
       
-      shadow_mask.composite!(
-        input_colorized, 
-        shadow[:blur], 
-        shadow[:blur], 
-        Magick::OverCompositeOp
-      )
+        shadow_mask.composite!(
+          input_colorized, 
+          shadow[:blur], 
+          shadow[:blur], 
+          Magick::OverCompositeOp
+        )
 
-      shadow_mask.matte = true
-      shadow_mask = shadow_mask.blur_image(shadow[:blur], shadow[:blur])
+        shadow_mask.matte = true
+        shadow_mask = shadow_mask.blur_image(shadow[:blur], shadow[:blur])
    
-      output = Magick::Image.new(width, height){
-        self.background_color = shadow[:color]
-      }
-      inverse_shadow_mask = shadow_mask.negate
-      inverse_shadow_mask.matte = false
+        output = Magick::Image.new(width, height){
+          self.background_color = shadow[:color]
+        }
+        inverse_shadow_mask = shadow_mask.negate
+        inverse_shadow_mask.matte = false
 
-      output.matte = true
+        output.matte = true
       
-      output.composite!(
-        inverse_shadow_mask, 
-        zero[0] + shadow[:left], zero[1] + shadow[:top], 
-        Magick::CopyOpacityCompositeOp
-      )
+        output.composite!(
+          inverse_shadow_mask, 
+          zero[0] + shadow[:left], zero[1] + shadow[:top], 
+          Magick::CopyOpacityCompositeOp
+        )
       
-      output.composite!(
-        input, 
-        zero[0] + shadow[:blur], zero[1] + shadow[:blur], 
-        Magick::OverCompositeOp
-      )
+        output.composite!(
+          input, 
+          zero[0] + shadow[:blur], zero[1] + shadow[:blur], 
+          Magick::OverCompositeOp
+        )
 
-      output.trim
+        output.trim
+      end
+
+      def rotate_render(input, angle)
+        input.matte = true
+        input.rotate!(angle)
+        input
+      end
     end
-
-
-    def rotate_render(input, angle)
-      input.matte = true
-      input.rotate!(angle)
-      input
-    end
-
   end
 end
